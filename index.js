d7de190673a4a574e5e4e56332f3516ee004d268
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-04-18 23:45:27 -0300

    basic functionality done

--- index.js
+++ index.js
@@ -1,57 +1,91 @@
-class Task{
+class Task {
     id;
     description;
     completed;
+    deleted;
 
-    constructor(id,description,completed){
+    constructor(id, description, completed, deleted) {
         this.id = id;
         this.description = description;
         this.completed = completed;
+        this.deleted = deleted;
     }
 }
 
-let tasks = [new Task(1,"Go walk the dog",false),new Task(2,"Get the car washed",false),new Task(3,"Do homework",false)]
+let tasks = [new Task(1, "Go walk the dog", false, true), new Task(2, "Get the car washed", true, false), new Task(3, "Do homework", false, false)];
+let select = document.getElementById("taskTypeSelect");
 
-const addTask = (description) => {
-    tasks.push(new Task(tasks[tasks.length].id,description));
+const addTask = () => {
+    let inputElement = document.getElementById("description-input");
+    tasks.push(new Task(tasks[tasks.length -1].id +1, inputElement.value, false, false));
+    inputElement.value = ""
+    listTasks();
 }
 
 const deleteTask = (id) => {
-    tasks.splice(tasks.find((task) => task.id === id),1);
+    let task = document.getElementById("task-" + id);
+    tasks.filter((t) => t.id === id)[0].deleted = true;
+    task.classList.add("hidden");
 }
 
 const completeTask = (id) => {
-    tasks[id].completed = true;
+    let task = tasks.filter((t) => t.id === id)[0].completed = true;
+    task.completed = true;
+    let taskElement = document.getElementById("task-" + id);
+    taskElement.classList.add("hidden")
 }
 
 const listTasks = () => {
     const tasksContainer = document.getElementById("tasks-container");
+    while (tasksContainer.firstChild) {
+        tasksContainer.removeChild(tasksContainer.firstChild);
+    }
+    let completedTasks = select.value;
+    let addTaskContainer = document.getElementById("add-task-container");
+    completedTasks === "true" ? addTaskContainer.classList.add("hidden"): addTaskContainer.classList.remove("hidden");
+    const filteredTasks = tasks.filter((task) => completedTasks === "false" ? task.completed === false : task.completed === true)
 
-    for(let i = 0; i < tasks.length;i++){
-        const taskDiv = document.createElement("div")
-        taskDiv.classList.add("task")
-        const taskDescription = document.createElement("h3")
-        const textNode = document.createTextNode(tasks[i].description)
+    for (let i = 0; i < filteredTasks.length; i++) {
+        if (filteredTasks[i].deleted) {
+            continue;
+        }
+        let task = filteredTasks[i];
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add("task");
+        taskDiv.setAttribute("id", "task-" + task.id);
+        const taskDescription = document.createElement("h3");
+        const textNode = document.createTextNode(task.description);
         taskDescription.appendChild(textNode);
 
-        const buttonContainer = document.createElement("div")
+        const taskCompleted = document.createElement("p");
+        const taskCompletedTextNode = document.createTextNode(completedTasks === "true" ? "Completed" : "Pending");
+        taskCompleted.appendChild(taskCompletedTextNode);
 
-        const deleteButton = document.createElement("button")
-        const deleteButtonText = document.createTextNode("Delete")
-        deleteButton.appendChild(deleteButtonText)
-        deleteButton.addEventListener("click",() => deleteTask(tasks[i].id))
+        const buttonContainer = document.createElement("div");
 
-        const doneButton = document.createElement("button")
-        const doneButtonText = document.createTextNode("Done")
-        doneButton.appendChild(doneButtonText)
-    
-        taskDiv.appendChild(taskDescription)
-        buttonContainer.appendChild(deleteButton)
-        buttonContainer.appendChild(doneButton)
+        const deleteButton = document.createElement("button");
+        const deleteButtonText = document.createTextNode("Delete");
+        deleteButton.appendChild(deleteButtonText);
+        deleteButton.addEventListener("click", () => deleteTask(task.id));
+
+        const doneButton = document.createElement("button");
+        const doneButtonText = document.createTextNode("Done");
+        doneButton.appendChild(doneButtonText);
+        doneButton.addEventListener("click",() => completeTask(task.id))
+
+        taskDiv.appendChild(taskDescription);
+        taskDiv.appendChild(taskCompleted);
+        buttonContainer.appendChild(deleteButton);
+        completedTasks === "true" ? taskCompleted.classList.add("green") :buttonContainer.appendChild(doneButton);
         taskDiv.appendChild(buttonContainer);
 
-        tasksContainer.appendChild(taskDiv)
+        tasksContainer.appendChild(taskDiv);
     }
 }
 
+select.addEventListener("change", () => {
+    listTasks();
+})
+
+document.getElementById("add-task-button").addEventListener("click", () => {addTask()})
 listTasks();
